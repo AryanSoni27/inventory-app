66e7247c0b3da69fae322a23bca7ee8234147cb5
AryanSoni27 <aryansoni33635@gmail.com>
2026-06-23 20:15:24 +0530

    Set JVM and Hibernate timezone to IST (Asia/Kolkata) for correct timestamps

--- inventory-backend/src/main/java/com/inventory/InventoryApplication.java
+++ inventory-backend/src/main/java/com/inventory/InventoryApplication.java
@@ -3,10 +3,20 @@ package com.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class InventoryApplication {
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(InventoryApplication.class, args);
     }
 }
